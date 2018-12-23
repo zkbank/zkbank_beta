b1c264751b973b8fd736fb088fb11044f0ed687a
Denis Gavrilin <denis020611@gmail.com>
2018-12-23 15:39:26 +0800

    test listadresses

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/test', function (req,res) {
-  res.send('test')
+  res.send('test2')
 })
 
 router.get('/newAddress', function(req, res) {
