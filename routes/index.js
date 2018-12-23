ccad189dd4a98572668291dc5624fa261197065d
Denis Gavrilin <denis020611@gmail.com>
2018-12-23 15:18:09 +0800

    test listadresses

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/', function (req,res) {
+router.get('/test', function (req,res) {
   res.send('test')
 })
 
