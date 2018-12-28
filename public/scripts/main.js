d852d34e77073a22e3339ca631e5b83f075fecb8
Paol T <pavel.t@webfree.io>
2018-12-28 08:15:46 +0800

    sheeet

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -22,8 +22,8 @@ $('*[data-modal-close]').click(function(){
 
 function tab_show(id, group)
 {
-  $('a[data-tab-open=' + id + ']').parent().find('a').removeClass('selected');
-  $('a[data-tab-open=' + id + ']').addClass('selected');
+  $('a[data-tab-group=' + group + '][data-tab-open=' + id + ']').parent().find('a').removeClass('selected');
+  $('a[data-tab-group=' + group + '][data-tab-open=' + id + ']').addClass('selected');
   $('ul[data-tab-group=' + group + '] > li').hide();
   $('ul[data-tab-group=' + group + '] > li[data-tab=' + id + ']').show();
 }
